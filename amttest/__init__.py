bb45200a4c2f32bb94fcf6c42f98bebddfb63635
Alex Balderson <alex@menutrinfo.com>
2018-04-16 18:45:32 -0600

    renamed repo

--- amttest/__init__.py
+++ amttest/__init__.py
@@ -3,7 +3,7 @@ TODO
 """
 
 __description__ = 'API for amtgard test package'
-__url__ = 'https://github.com/asbalderson/test_api'
+__url__ = 'https://github.com/asbalderson/amttest'
 __version__ = '0.0.1'
 __author__ = 'Alex Balderson'
 __author_email__ = 'mr.balderson.alex@gmail.com'
