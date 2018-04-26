b98dea30ad195d211a6e3c6ab43d19d0b3c65401
Alex Balderson <alex@menutrinfo.com>
2018-04-26 17:03:27 -0600

    adding routes for sections, currently untested

--- amttest/routes/section.py
+++ amttest/routes/section.py
@@ -1,6 +1,20 @@
+from collections import defaultdict
 from flask import jsonify, request, make_response, Blueprint
+from sqlalchemy import inspect
+
+import logging
+import json
+
 from ..helpers.bphandler import BPHandler
 from ..helpers.token import get_token, check_token
+
+from ..database import db
+from ..database.tables.answer import Answer
+from ..database.tables.section import Section
+from ..database.tables.question import Question
+from ..database.utils import table2dict
+
+from ..errors.badrequest import BadRequest
 
 sections = [
     {
@@ -18,16 +32,37 @@ sections = [
 SECTION_BP = Blueprint('sections', __name__)
 BPHandler.add_blueprint(SECTION_BP, url_prefix='/amttest/api')
 
-#i think this is all admin stuff?
-@SECTION_BP.route('/tests/<int:test_id>/sections/', methods=['POST'])
+
+@SECTION_BP.route('/tests/<int:test_id>/sections', methods=['POST'])
 def create_section(test_id):
     """
     creates a new section
     """
     check_token(get_token(request))
-    message = {}
-    message['section_id'] = 1234
-    return make_response(jsonify(message), 201)
+    payload_raw = request.data.decode()
+    payload = json.loads(payload_raw)
+    fields = {'testid':test_id}
+    for column in payload.keys():
+        if column not in inspect(Section).mapper.column_attrs:
+            continue
+        fields[column] = payload[column]
+    section = Section(**fields)
+    db.session.add(section)
+    db.session.commit()
+    db.session.refresh(section)
+
+    return make_response(jsonify(table2dict(section)), 201)
+
+
+@SECTION_BP.route('/tests/<int:test_id>/sections', methods=['GET'])
+def get_test_sections(test_id):
+    section_data = Section.query.filter_by(archive=False, testid=test_id).all()
+
+    section_list = []
+    for section in section_data:
+        section.append(table2dict(section))
+
+    return make_response(jsonify(section_list), 200)
 
 
 @SECTION_BP.route('/sections', methods=['GET'])
@@ -46,8 +81,13 @@ def get_all_sections():
     'active_qestions': 5
     }
     """
-    global sections
-    return make_response(jsonify(sections), 200)
+    section_data = Section.query.filter_by(archive=False).all()
+
+    section_list = []
+    for section in section_data:
+        section.append(table2dict(section))
+
+    return make_response(jsonify(section_list), 200)
 
 
 @SECTION_BP.route('/sections/<int:section_id>', methods = ['GET'])
@@ -57,31 +97,24 @@ def get_section(section_id):
     actually dont know if this is needed, might be able to just
     get questions for a section id
     """
-    message ={
-            'name': 'Rules of Engagement',
-            'section_id': 2,
-            'active_questions': 5,
-            'questions': [
-                {
-                    'question_id as int':{
-                        'question': 'what are we doing',
-                        'answers':[
-                            {
-                            'answer_id': 5,
-                            'answer': 'no one knows',
-                            'correct': False
-                            },
-                            {
-                                'answer_id': 6,
-                                'answer': 'having a laugh at alex\'s expense',
-                                'correct': True
-                            },
-                        ]
-                    }
-                }
-            ]
-        }
-    return make_response(jsonify(message), 200)
+
+    section = Section.query.filter_by(archive=False, sectionid=section_id).first()
+    if not section:
+        raise BadRequest('Section does not exist')
+
+    return_dict = table2dict(section)
+
+    questions = Question.query.filter_by(archive=False, sectionid=section.sectionid)
+    return_dict['questions'] = []
+    for question in questions:
+        question_dict = table2dict(question)
+        question_dict['answers'] = []
+        answers = Answer.query.filter_by(archive=False, questionid=question.questionid)
+        for answer in answers:
+            question_dict['answers'].append(table2dict(answer))
+        return_dict['questions'].append(question_dict)
+
+    return make_response(jsonify(return_dict), 200)
 
 
 @SECTION_BP.route('/sections/<int:section_id>', methods = ['PUT'])
@@ -90,26 +123,28 @@ def update_section(section_id):
     this is used to change the number of questions usd for a section
     """
     check_token(get_token(request))
-    message ={
-            'name': 'Rules of Engagement',
-            'section_id': 2,
-            'active_questions': 5
-        }
-    return make_response(jsonify(message), 201)
+    payload_raw = request.data.decode()
+    payload = json.loads(payload_raw)
+    section = Section.query.filter_by(archive=False, sectionid=section_id).first()
+    if not section:
+        raise BadRequest('Section not found')
+
+    for field in payload.keys():
+        if field in inspect(Section).mapper.column_attrs:
+            setattr(section, field, payload[field])
+
+    db.session.commit()
+
+    return make_response('', 204)
 
 @SECTION_BP.route('/sections/<int:section_id>', methods = ['DELETE'])
 def delete_section(section_id):
     """
     removes a section, tests calling removed section will be invalid
     should produce error if section is in use
-    AKA there is a new GMR.  deleted sections will be archived,
-    so either moved to a new database, or set with an archive flag
-    eventually we will need a way to query the archive, but i bet
-    i could mirror most of the calls and add a /archive/ infront of
-    everything
     """
     check_token(get_token(request))
-    result = {}
-    result['success'] = True
-    result['message'] = 'section %s Deleted' % section_id
-    return make_response(jsonify(result), 200)
+    section = Section.query.filter_by(archive=False, sectionid=section_id).first()
+    section.archive = False
+    db.session.commit()
+    return make_response('', 204)
