352ad44e7c35c2869662d7f4bc1c00a845a62f40
Alex Balderson <alex@menutrinfo.com>
2018-05-03 21:35:24 -0600

    fbuser should be text

--- amttest/database/tables/user.py
+++ amttest/database/tables/user.py
@@ -4,7 +4,7 @@ from .. import db
 class User(db.Model):
     __tablename__ = 'user'
     userid = db.Column(db.Integer, primary_key=True, nullable=False)
-    fbuserid = db.Column(db.Integer, nullable=False)
+    fbuserid = db.Column(db.Text, nullable=False)
     amt_name = db.Column(db.Text, default='')
     name = db.Column(db.Text, nullable=False)
     email = db.Column(db.Text, nullable=False)
